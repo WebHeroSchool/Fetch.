05dace5bed2794533e08e9410ce96b7b11880fb6
Nataliia Chuienko <bvadim630@gmail.com>
2021-04-24 19:51:36 +0300

    fix

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-let response = document.createElement('div');
-fetch('https://api.github.com/users/NataliiaChuienko')
-.then(res => res.json)
-.then(json => response.innerHTML = json.name)
-.then(json => response.innerHTML = json.bio)
-.then(json => response.innerHTML = json.avatar_url)
-.catch(err => console.log(err));
+let body = document.body;
+let url = 'https://api.github.com/users/NataliiaChuienko';
+
+
+fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json.avatar_url);
+        console.log(json.name);
+        console.log(json.bio);
+   
+
+        let img = document.createElement('img');
+        if (json.avatar_url != null) {
+        img.src = json.avatar_url;
+        } else {
+        img.src = 'Информация о пользователе недоступна'
+         }
+        body.append(img);
+
+        let name = document.createElement('p');
+        if (json.name != null) {
+            name.innerHTML = json.name;
+        } else {
+            name.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(name);
+
+        let bio = document.createElement('p');
+        if (json.bio != null) {
+            bio.innerHTML = json.bio;
+        } else {
+            bio.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(bio);
+
+    })
+    .catch(err => console.log(err));
+
+
+
